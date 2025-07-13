836dcbbece34652006d3cf12b3169b501228feca
root <mantillajulian234@gmail.com>
2025-07-13 13:58:54 -0500

    continua mejorando la imagen del prototipo de la API

--- my-web-api/main.py
+++ my-web-api/main.py
@@ -1,4 +1,20 @@
-nombre = 'Gustavo'
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
-print(f'hello {nombre}, este es un mensaje desde: my-web-api')
+# Create a FastAPI application instance
+app = FastAPI()
 
+# Define your first API endpoint (route)
+@app.get("/", tags=['root'])
+async def read_root():
+    nombre = 'Gustavo'
+    html_content = f'<h1>Hello {nombre} From API, this is root</h1>'
+    return HTMLResponse(html_content)
+
+
+# Define your first API endpoint (route)
+@app.get("/cat", tags=['cat'])
+async def read_root():
+    nombre = 'Cat'
+    html_content = f'<h1>Hello {nombre} From API, this is CAT Endpoint</h1>'
+    return HTMLResponse(html_content)
