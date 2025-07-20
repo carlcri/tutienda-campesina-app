d1c2fd3a860226b241922b7d33927516afae4f95
julianC <mantillajulian234@gmail.com>
2025-07-19 23:00:31 -0500

    mejoro frotend formulario buscar por id, mensaje error, falta documentar

--- README.md
+++ README.md
@@ -4,5 +4,28 @@ tienda campesina
 Cambio 1
 
 
+```mermaid
+graph TD
+    A[fetch(api_URL)] --> B{Server Responds};
+    B -- Status 200-299 --> C{response.ok is true};
+    C --> D[return response.json()];
+    D -- Parsed Client Data --> E[Second .then(client => ...)];
+
+    B -- Status 4xx or 5xx --> F{response.ok is false};
+    F --> G[return response.json()];
+    G -- Parsed Error Data --> H[Nested .then(errorData => ...)];
+    H --> I[throw new Error(...)];
+    I --> J[Outer .catch(error => ...)];
 
 
+graph TD
+    A[fetch(api_URL)] --> B{Server Responds};
+    B -- Status 200-299 --> C{response.ok is true};
+    C --> D[return response.json()];
+    D -- Parsed Client Data --> E[Second .then(client => ...)];
+
+    B -- Status 4xx or 5xx --> F{response.ok is false};
+    F --> G[return response.json()];
+    G -- Parsed Error Data --> H[Nested .then(errorData => ...)];
+    H --> I[throw new Error(...)];
+    I --> J[Outer .catch(error => ...)];
--- web-page/JS/my-script.js
+++ web-page/JS/my-script.js
@@ -1,17 +1,11 @@
 'use strict'
 
 $(document).ready(function(){
-    console.log('Jquery ha cargado. Feliz');
-    // var aux = $('#clientForm').eq(0);
-    // console.log(aux);
-    // console.log(aux.html());
-
-    var counter=0;
+    console.log('Jquery ha cargado exitosamente');
+    var error_message = '';
 
     $('#searchClientButton').click(function(e){ 
         e.preventDefault();
-        counter++;
-        console.log(counter);
 
         let client_id = $('#clientId').val();
         client_id = parseInt(client_id)
@@ -24,67 +18,50 @@ $(document).ready(function(){
 
         const api_URL = `http://localhost/api/client/${client_id}`;
         fetch(api_URL)
-        .then(function(data){
-            return data.json();
+        .then(function(response){
+            console.log(`ok: ${response.ok} | status code: ${response.status}`)
+
+            if(!response.ok){
+                return response.json().then(function(apiErrorResponse){
+//                    console.log(apiErrorResponse);
+                    error_message = `status_code:${response.status} | details: ${apiErrorResponse.detail.message}`    
+                    let error = new Error(error_message);
+                    error.status_code = response.status;
+                    error.details = apiErrorResponse.detail.message; 
+                    throw error;
+                })
+            }
+
+            return response.json();
         })
         .then(function(client){
-            console.log(client);
-            console.log(client.DOB);
-            console.log(client.address);
-            console.log(client.lastName);
+            console.log(`cliente con id:${client.id} encontrado`);
+            $('#result-container').show();
+            $('.message').show();
+            $('#error-message').hide();
+//            $('#clientForm').hide();
+            $('#initial-message').hide();
+
+
+            $('#client-id-display').html(client.id);
+            $('#client-name-display').html(client.name);
+            $('#client-lastname-display').html(client.lastName);
+            $('#client-dob-display').html(client.DOB);            
+
+        })
+        .catch(function(error){
+            //<p id="error-message" class="message"></p>
+            console.log('hola error catch...');  
+            console.log(error_message);
+            console.log(error.status_code, error.details);
+
+            $('#result-container').show();
+            $('.message').show();
+            $('#client-details').hide();
+//            $('#clientForm').hide();
+
+            $('#error-message').html(`<p>${error_message}</p>`);
+
         })  
     });
 })
-
-// var usuarios = []
-// var usuarios_lista = document.querySelector('#usuarios-lista');
-// console.log(usuarios_lista.innerHTML)
-
-
-// fetch('https://reqres.in/api/users?page=2', {
-//      headers: {
-//           'x-api-key': 'reqres-free-v1' 
-//      }
-// })
-//      .then(function(data) {
-//           return data.json();  // Convierte la respuesta a JSON
-//      })
-//      .then(function(users) {
-//           usuarios = users.data;  // Accede a la propiedad 'data'
-
-//           usuarios.forEach(function(element){
-//                let elemento_lista = document.createElement('li');
-//                elemento_lista.append(`${element.id}: ${element.first_name} ${element.email}`);
-//                usuarios_lista.append(elemento_lista);
-//           })
-// });
-
-
-// fetch('https://reqres.in/api/users/4', {
-//      headers: {
-//           'x-api-key': 'reqres-free-v1' 
-//      }
-// })
-//      .then(function(data) {
-//           return data.json();  // Convierte la respuesta a JSON
-//      })
-//      .then(function(user){
-//           let usuario = []
-//           usuario = user.data;
-//           console.log(usuario);
-//           console.log(usuario.first_name, usuario.email, usuario.id)
-//      })
-
-
- 
-// fetch('http://localhost/api/client/10')
-//        .then(function(data){
-//             return data.json();
-//      })
-//      .then(function(client){
-//          console.log(client);
-//           console.log(client.DOB);
-//           console.log(client.address);
-//           console.log(client.lastName);
-//      })     
-
